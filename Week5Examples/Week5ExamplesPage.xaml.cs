c5b6a36109630aacb1828fe36bf7b7f84055f369
Thomas Desmond <thomasdes533@gmail.com>
2017-09-25 17:40:08 -0700

    Initial Commit

---
+++ Week5Examples/Week5ExamplesPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Week5Examples
+{
+    public partial class Week5ExamplesPage : ContentPage
+    {
+        public Week5ExamplesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
